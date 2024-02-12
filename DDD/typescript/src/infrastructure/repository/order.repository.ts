a6f0b82453f8a5b8960f01e60d949700bf06808c
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2024-02-12 00:32:06 -0300

    Add order-repository iterface

--- DDD/typescript/src/infrastructure/repository/order.repository.ts
+++ DDD/typescript/src/infrastructure/repository/order.repository.ts
@@ -1,9 +1,10 @@
 import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create({
       id: entity.id,
@@ -21,5 +22,17 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }]
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    throw Error("Method not implemented."); 
+  }
+
+  async find(id: string): Promise<Order> {
+    throw Error("Method not implemented."); 
+  }
+
+  async findAll(): Promise<Order[]> {
+    throw Error("Method not implemented."); 
   }
 }
