bc767f94c7a2ce169ecd41b079d95dad11486dcd
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2024-03-18 15:41:34 -0300

    implementacao dos metodos da order repository

--- DDD/typescript/src/infrastructure/repository/order.repository.spec.ts
+++ DDD/typescript/src/infrastructure/repository/order.repository.spec.ts
@@ -186,4 +186,95 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should find a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRespository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderResult = await orderRepository.find(order.id)
+
+    expect(order).toStrictEqual(orderResult);
+
+  });
+
+  it("should throw an error when order is not found", async () => {
+    const orderRepository = new OrderRepository();
+
+    expect(async () => {
+      await orderRepository.find("456ABC");
+    }).rejects.toThrow("Order not found");
+  });
+
+  it("should find all orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer1 = new Customer("123", "Customer 1");
+    const address1 = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer1.Address = address1;
+    // customer1.addRewardPoints(10);
+    // customer1.activate();
+    
+    const customer2 = new Customer("456", "Customer 2");
+    const address2 = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    customer2.Address = address2;
+    // customer2.addRewardPoints(20);
+
+    await customerRepository.create(customer1);
+    await customerRepository.create(customer2);
+
+    const productRepository = new ProductRespository();
+    const product1 = new Product("123", "Product 1", 10);
+    const product2 = new Product("456", "Product 2", 20);
+
+    await productRepository.create(product1);
+    await productRepository.create(product2);
+    
+    const orderItem1 = new OrderItem(
+      "1",
+      product1.name,
+      product1.price,
+      product1.id,
+      2
+    );
+
+    const orderItem2 = new OrderItem(
+      "2",
+      product2.name,
+      product2.price,
+      product2.id,
+      4
+    );
+
+    const order1 = new Order("123", "123", [orderItem1]);
+    const order2 = new Order("456", "456", [orderItem2]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order1);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+
+    expect(orders).toHaveLength(2);
+    expect(orders).toContainEqual(order1);
+    expect(orders).toContainEqual(order2);
+  });
 });
--- DDD/typescript/src/infrastructure/repository/order.repository.ts
+++ DDD/typescript/src/infrastructure/repository/order.repository.ts
@@ -1,7 +1,12 @@
+import Address from "../../domain/entity/address";
+import Customer from "../../domain/entity/customer";
 import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
 import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import CustomerModel from "../db/sequelize/model/customer.model";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
+import CustomerRepository from "./customer.repository";
 
 
 export default class OrderRepository implements OrderRepositoryInterface {
@@ -36,7 +41,7 @@ export default class OrderRepository implements OrderRepositoryInterface {
       },
     );
     
-    entity.items.forEach(item => {
+   await Promise.all(entity.items.map(async item => {
        OrderItemModel.update({
         name: item.name,
         price: item.price,
@@ -48,16 +53,59 @@ export default class OrderRepository implements OrderRepositoryInterface {
           id: item.id
         }
       })
-    });
+    })); 
   
 
   }
 
   async find(id: string): Promise<Order> {
-    throw Error("Method not implemented."); 
+    let orderModel;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: ["items"],
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const orderItem: OrderItem[] = []
+
+    orderModel.items.forEach((item) => {
+      orderItem.push(
+        new OrderItem(
+          item.id, 
+          item.name, 
+          item.price, 
+          item.product_id, 
+          item.quantity))
+    });
+
+    const order = new Order(orderModel.id, orderModel.customer_id, orderItem)
+    return order
   }
 
   async findAll(): Promise<Order[]> {
-    throw Error("Method not implemented."); 
+    const orderModels = await OrderModel.findAll({include: ["items"]});
+    const orders = orderModels.map((orderModels) => {
+      let orderItems: OrderItem[] = []
+      orderModels.items.forEach((item) => {
+        orderItems.push(
+          new OrderItem(
+            item.id, 
+            item.name, 
+            item.price, 
+            item.product_id, 
+            item.quantity))
+      });
+  
+      let order = new Order(orderModels.id, orderModels.customer_id, orderItems)
+      return order;
+    });
+
+    return orders
   }
 }
